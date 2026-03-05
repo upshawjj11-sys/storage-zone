25cc867ce9d61866bbbe5a4da457d62805cce144
Jeremy Upshaw <upshawjj11@gmail.com>
2026-03-05 22:59:07 +0000

    File changes

--- src/components/home/DynamicIcon.jsx
+++ src/components/home/DynamicIcon.jsx
@@ -1,25 +1,62 @@
-import { Shield, Lock, ShieldCheck, Key, Eye, Clock, Calendar, CalendarCheck, Timer, MapPin, Map, Navigation, Compass, Home, Building, Building2, Warehouse, Star, Heart, ThumbsUp, Award, Trophy, BadgeCheck, Check, CheckCircle, CheckCircle2, Truck, Car, Package, Package2, PackageCheck, Box, Boxes, Tag, Tags, DollarSign, CreditCard, Banknote, Coins, Percent, BadgeDollarSign, Phone, Mail, MessageCircle, MessageSquare, Bell, BellRing, Leaf, Sun, Moon, Wind, Snowflake, Flame, Zap, Wifi, Plug, Battery, Users, User, UserCheck, UserPlus, Wrench, Settings, Settings2, Hammer, ArrowRight, ArrowUp, ArrowDown, ChevronRight, Plus, X, Minus, RefreshCw, Camera, Image, FileText, Clipboard, ClipboardCheck, BookOpen, Globe, Info, HelpCircle, AlertCircle } from "lucide-react";
+import {
+  Shield, Lock, ShieldCheck, ShieldAlert, ShieldOff, Key, Eye, EyeOff, Clock, Clock1, Clock2, Clock3, Clock4, Clock5, Clock6, Clock7, Clock8, Clock9, Clock10, Clock11, Clock12, Calendar, CalendarCheck, CalendarDays, CalendarClock, CalendarRange, CalendarX, CalendarPlus, Timer, TimerOff, TimerReset, Hourglass, AlarmClock, AlarmClockOff, Watch,
+  MapPin, Map, Navigation, Navigation2, Compass, Globe, Globe2, LocateFixed, Locate, LocateOff,
+  Home, Building, Building2, Warehouse, Store, Hotel, School, Hospital, Church, Landmark, Castle, Factory,
+  Star, Heart, ThumbsUp, ThumbsDown, Award, Trophy, BadgeCheck, Badge, Medal, Crown, Ribbon, Gift, PartyPopper, Sparkles,
+  Check, CheckCircle, CheckCircle2, CheckSquare, XCircle, XSquare, MinusCircle, Circle,
+  Truck, Car, Bus, Bike, Plane, Ship, Train, Forklift, Ambulance,
+  Package, Package2, PackageCheck, PackageOpen, PackageMinus, PackagePlus, Box, Boxes, Archive, ArchiveRestore, ArchiveX, Luggage, ShoppingBag, ShoppingCart, ShoppingBasket, Backpack,
+  Tag, Tags, DollarSign, CreditCard, Banknote, Coins, Percent, BadgeDollarSign, Receipt, Wallet, PiggyBank, HandCoins, CircleDollarSign,
+  Phone, PhoneCall, PhoneIncoming, PhoneOutgoing, PhoneMissed, PhoneOff, PhoneForwarded, Mail, MailOpen, MailCheck, MailPlus, MailMinus, MailX, MessageCircle, MessageSquare, MessageSquareDashed, MessagesSquare, Send, Inbox, MessageCircleDashed, MessageCirclePlus, Bell, BellRing, BellOff, BellPlus, BellMinus, Megaphone, Radio, Podcast, Headphones,
+  Leaf, Sun, Moon, Wind, Snowflake, Flame, Zap, ZapOff, Cloud, CloudRain, CloudSnow, CloudSun, CloudMoon, CloudLightning, Tornado, Waves, Droplets, Droplet, Thermometer, Umbrella, Rainbow, Trees, Flower, Flower2, Sprout, Mountain, MountainSnow, Sunset, Sunrise, Eclipse,
+  Wifi, WifiOff, Plug, PlugZap, Battery, BatteryCharging, BatteryFull, BatteryLow, BatteryMedium, BatteryWarning, Power, PowerOff, Cpu, Monitor, Laptop, Tablet, Smartphone, SmartphoneCharging, Tv, Projector, Mouse, Keyboard, Printer, ScanLine, QrCode, Barcode, HardDrive, HardDriveDownload, HardDriveUpload, Server, Database, DatabaseBackup, DatabaseZap, Bluetooth, BluetoothConnected, BluetoothOff,
+  Users, User, UserCheck, UserPlus, UserMinus, UserX, UserCog, UsersRound, PersonStanding, Baby,
+  Wrench, Settings, Settings2, Hammer, Drill, Shovel, Axe, Ruler, Scissors, Pipette, TestTube, TestTube2, FlaskConical, FlaskRound, Microscope, Telescope, Binoculars,
+  ArrowRight, ArrowLeft, ArrowUp, ArrowDown, ArrowUpRight, ArrowUpLeft, ArrowDownRight, ArrowDownLeft, ArrowRightLeft, ArrowUpDown, ChevronRight, ChevronLeft, ChevronUp, ChevronDown, ChevronsRight, ChevronsLeft, ChevronsUp, ChevronsDown, MoveRight, MoveLeft, MoveUp, MoveDown, Undo, Undo2, Redo, Redo2, RotateCcw, RotateCw, Repeat, Repeat1, Shuffle, RefreshCw, RefreshCcw,
+  Plus, X, Minus, Divide, Equal, Hash,
+  Camera, CameraOff, Image, ImagePlus, ImageOff, Images, Film, Clapperboard, Video, VideoOff, FileText, Clipboard, ClipboardCheck, ClipboardList, ClipboardPaste, ClipboardPlus, ClipboardX, BookOpen, Book, BookMarked, BookCopy, BookHeart, BookKey, BookLock, BookMinus, BookPlus, BookX, Bookmark, BookmarkPlus, BookmarkMinus, BookmarkCheck, FileImage, FileSpreadsheet, FileCode, FileSearch, FileDown, FileUp, Folder, FolderOpen, FolderPlus, FolderMinus, FolderX, FolderSearch, FolderHeart, FolderKey, FolderLock, FolderClosed, Paperclip, PenTool, Pen, Pencil, PencilLine, PencilRuler, Eraser, NotebookPen, Notebook, NotepadText, Stamp, Sticker, Type, Text, AlignLeft, AlignCenter, AlignRight, AlignJustify,
+  Info, HelpCircle, AlertCircle, AlertTriangle, AlertOctagon, OctagonAlert,
+  Search, SearchCheck, SearchCode, SearchX, ZoomIn, ZoomOut, Filter, FilterX,
+  Layout, LayoutGrid, LayoutList, LayoutPanelLeft, LayoutPanelTop, LayoutTemplate, LayoutDashboard, Grid2x2, Grid3x3, Table, TableProperties, List, ListOrdered, ListChecks, ListTree, ListPlus, ListTodo,
+  Link, Link2, Unlink, Unlink2, ExternalLink, Anchor, Share, Share2, Forward, Reply, ReplyAll, AtSign, Rss, SatelliteDish,
+  Music, Music2, Music3, Music4, Mic, Mic2, MicOff, Volume, Volume1, Volume2, VolumeX, Play, Pause, StopCircle, SkipForward, SkipBack, FastForward, Rewind,
+  Coffee, Utensils, UtensilsCrossed, ChefHat, Pizza, Sandwich, Salad, Soup, Apple, Banana, Cherry, Grape, Citrus, Carrot, Cookie,
+  Dumbbell, Footprints, Activity, HeartPulse, Stethoscope, Syringe, Pill, Bandage, Cross,
+  Layers, Layers2, Layers3, SquareStack, PanelTop, PanelBottom, PanelLeft, PanelRight, Sidebar, SidebarOpen, SidebarClose,
+  MoreHorizontal, MoreVertical, Ellipsis, Accessibility, Contrast, Code, Code2, Terminal, Bug, Webhook,
+  Fingerprint, ScanFace, ShieldEllipsis, Unlock, BanIcon, CircleOff,
+} from "lucide-react";
 
-const ICONS = {
-  Shield, Lock, ShieldCheck, Key, Eye, Clock, Calendar, CalendarCheck, Timer,
-  MapPin, Map, Navigation, Compass, Home, Building, Building2, Warehouse,
-  Star, Heart, ThumbsUp, Award, Trophy, BadgeCheck,
-  Check, CheckCircle, CheckCircle2,
-  Truck, Car, Package, Package2, PackageCheck, Box, Boxes,
-  Tag, Tags, DollarSign, CreditCard, Banknote, Coins, Percent, BadgeDollarSign,
-  Phone, Mail, MessageCircle, MessageSquare, Bell, BellRing,
-  Leaf, Sun, Moon, Wind, Snowflake, Flame, Zap, Wifi, Plug, Battery,
-  Users, User, UserCheck, UserPlus,
-  Wrench, Settings, Settings2, Hammer,
-  ArrowRight, ArrowUp, ArrowDown, ChevronRight,
-  Plus, X, Minus, RefreshCw,
-  Camera, Image, FileText, Clipboard, ClipboardCheck, BookOpen,
-  Globe, Info, HelpCircle, AlertCircle,
+export const ICONS = {
+  Shield, Lock, ShieldCheck, ShieldAlert, ShieldOff, Key, Eye, EyeOff, Fingerprint, ScanFace, ShieldEllipsis, Unlock,
+  Clock, Clock1, Clock2, Clock3, Clock4, Clock5, Clock6, Clock7, Clock8, Clock9, Clock10, Clock11, Clock12, Calendar, CalendarCheck, CalendarDays, CalendarClock, CalendarRange, CalendarX, CalendarPlus, Timer, TimerOff, TimerReset, Hourglass, AlarmClock, AlarmClockOff, Watch,
+  MapPin, Map, Navigation, Navigation2, Compass, Globe, Globe2, LocateFixed, Locate, LocateOff,
+  Home, Building, Building2, Warehouse, Store, Hotel, School, Hospital, Church, Landmark, Castle, Factory,
+  Star, Heart, ThumbsUp, ThumbsDown, Award, Trophy, BadgeCheck, Badge, Medal, Crown, Ribbon, Gift, PartyPopper, Sparkles,
+  Check, CheckCircle, CheckCircle2, CheckSquare, XCircle, XSquare, MinusCircle, Circle,
+  Truck, Car, Bus, Bike, Plane, Ship, Train, Forklift, Ambulance,
+  Package, Package2, PackageCheck, PackageOpen, PackageMinus, PackagePlus, Box, Boxes, Archive, ArchiveRestore, ArchiveX, Luggage, ShoppingBag, ShoppingCart, ShoppingBasket, Backpack,
+  Tag, Tags, DollarSign, CreditCard, Banknote, Coins, Percent, BadgeDollarSign, Receipt, Wallet, PiggyBank, HandCoins, CircleDollarSign,
+  Phone, PhoneCall, PhoneIncoming, PhoneOutgoing, PhoneMissed, PhoneOff, PhoneForwarded, Mail, MailOpen, MailCheck, MailPlus, MailMinus, MailX, MessageCircle, MessageSquare, MessageSquareDashed, MessagesSquare, Send, Inbox, MessageCircleDashed, MessageCirclePlus, Bell, BellRing, BellOff, BellPlus, BellMinus, Megaphone, Radio, Podcast, Headphones,
+  Leaf, Sun, Moon, Wind, Snowflake, Flame, Zap, ZapOff, Cloud, CloudRain, CloudSnow, CloudSun, CloudMoon, CloudLightning, Tornado, Waves, Droplets, Droplet, Thermometer, Umbrella, Rainbow, Trees, Flower, Flower2, Sprout, Mountain, MountainSnow, Sunset, Sunrise, Eclipse,
+  Wifi, WifiOff, Plug, PlugZap, Battery, BatteryCharging, BatteryFull, BatteryLow, BatteryMedium, BatteryWarning, Power, PowerOff, Cpu, Monitor, Laptop, Tablet, Smartphone, SmartphoneCharging, Tv, Projector, Mouse, Keyboard, Printer, ScanLine, QrCode, Barcode, HardDrive, HardDriveDownload, HardDriveUpload, Server, Database, DatabaseBackup, DatabaseZap, Bluetooth, BluetoothConnected, BluetoothOff,
+  Users, User, UserCheck, UserPlus, UserMinus, UserX, UserCog, UsersRound, PersonStanding, Baby,
+  Wrench, Settings, Settings2, Hammer, Drill, Shovel, Axe, Ruler, Scissors, Pipette, TestTube, TestTube2, FlaskConical, FlaskRound, Microscope, Telescope, Binoculars,
+  ArrowRight, ArrowLeft, ArrowUp, ArrowDown, ArrowUpRight, ArrowUpLeft, ArrowDownRight, ArrowDownLeft, ArrowRightLeft, ArrowUpDown, ChevronRight, ChevronLeft, ChevronUp, ChevronDown, ChevronsRight, ChevronsLeft, ChevronsUp, ChevronsDown, MoveRight, MoveLeft, MoveUp, MoveDown, Undo, Undo2, Redo, Redo2, RotateCcw, RotateCw, Repeat, Repeat1, Shuffle, RefreshCw, RefreshCcw,
+  Plus, X, Minus, Divide, Equal, Hash,
+  Camera, CameraOff, Image, ImagePlus, ImageOff, Images, Film, Clapperboard, Video, VideoOff, FileText, Clipboard, ClipboardCheck, ClipboardList, ClipboardPaste, ClipboardPlus, ClipboardX, BookOpen, Book, BookMarked, BookCopy, BookHeart, BookKey, BookLock, BookMinus, BookPlus, BookX, Bookmark, BookmarkPlus, BookmarkMinus, BookmarkCheck, FileImage, FileSpreadsheet, FileCode, FileSearch, FileDown, FileUp, Folder, FolderOpen, FolderPlus, FolderMinus, FolderX, FolderSearch, FolderHeart, FolderKey, FolderLock, FolderClosed, Paperclip, PenTool, Pen, Pencil, PencilLine, PencilRuler, Eraser, NotebookPen, Notebook, NotepadText, Stamp, Sticker, Type, Text, AlignLeft, AlignCenter, AlignRight, AlignJustify,
+  Info, HelpCircle, AlertCircle, AlertTriangle, AlertOctagon, OctagonAlert, BanIcon, CircleOff,
+  Search, SearchCheck, SearchCode, SearchX, ZoomIn, ZoomOut, Filter, FilterX,
+  Layout, LayoutGrid, LayoutList, LayoutPanelLeft, LayoutPanelTop, LayoutTemplate, LayoutDashboard, Grid2x2, Grid3x3, Table, TableProperties, List, ListOrdered, ListChecks, ListTree, ListPlus, ListTodo,
+  Link, Link2, Unlink, Unlink2, ExternalLink, Anchor, Share, Share2, Forward, Reply, ReplyAll, AtSign, Rss, SatelliteDish,
+  Music, Music2, Music3, Music4, Mic, Mic2, MicOff, Volume, Volume1, Volume2, VolumeX, Play, Pause, StopCircle, SkipForward, SkipBack, FastForward, Rewind,
+  Coffee, Utensils, UtensilsCrossed, ChefHat, Pizza, Sandwich, Salad, Soup, Apple, Banana, Cherry, Grape, Citrus, Carrot, Cookie,
+  Dumbbell, Footprints, Activity, HeartPulse, Stethoscope, Syringe, Pill, Bandage, Cross,
+  Layers, Layers2, Layers3, SquareStack, PanelTop, PanelBottom, PanelLeft, PanelRight, Sidebar, SidebarOpen, SidebarClose,
+  MoreHorizontal, MoreVertical, Ellipsis, Accessibility, Contrast, Code, Code2, Terminal, Bug, Webhook,
 };
 
-export default function DynamicIcon({ name, className }) {
+export default function DynamicIcon({ name, className, style }) {
   const Icon = name && ICONS[name] ? ICONS[name] : Shield;
-  return <Icon className={className} />;
+  return <Icon className={className} style={style} />;
 }
-
-export { ICONS };
