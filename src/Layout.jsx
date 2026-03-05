6798aee7a2976cad8bb85120b80cc26eb4027cfc
Jeremy Upshaw <upshawjj11@gmail.com>
2026-03-05 22:21:20 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -80,7 +80,7 @@ export default function Layout({ children, currentPageName }) {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-1 pt-16 md:pt-20">{children}</main>
+      <main className="flex-1">{children}</main>
       <Footer />
     </div>
   );
