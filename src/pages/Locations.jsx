766353ee376110f32d8c0ff139d572a7e7d9e9d4
Jeremy Upshaw <upshawjj11@gmail.com>
2026-03-07 17:46:27 +0000

    File changes

--- src/pages/Locations.jsx
+++ src/pages/Locations.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { createPageUrl } from "@/utils";
 import { useQuery } from "@tanstack/react-query";
