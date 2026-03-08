16728f51113dbabd5abf834e16d9dc20840cc903
Jeremy Upshaw <upshawjj11@gmail.com>
2026-03-08 00:55:00 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -98,24 +98,14 @@ export default function Layout({ children, currentPageName }) {
   // Extract facilityId from URL for facility-specific popup matching
   const facilityId = new URLSearchParams(window.location.search).get("id");
 
-  // Serve FacilityPage for /locations/[state]/[city]/[slug]/ URLs
-  if (isFacilitySlugPage) {
-    return (
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-1"><FacilityPage /></main>
-        <Footer />
-        <PopupRenderer currentPageName="FacilityPage" facilityId={null} />
-      </div>
-    );
-  }
+  const activePageName = isFacilitySlugPage ? "FacilityPage" : currentPageName;
 
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-1">{children}</main>
       <Footer />
-      <PopupRenderer currentPageName={currentPageName} facilityId={facilityId} />
+      <PopupRenderer currentPageName={activePageName} facilityId={facilityId} />
     </div>
   );
 }
