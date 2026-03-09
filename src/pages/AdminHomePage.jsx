7efad57bf294a946e420578e7469eb117fec657a
Jeremy Upshaw <upshawjj11@gmail.com>
2026-03-09 18:12:40 +0000

    File changes

--- src/pages/AdminHomePage.jsx
+++ src/pages/AdminHomePage.jsx
@@ -782,6 +782,7 @@ export default function AdminHomePage() {
             )}
           </CardContent>
         </Card>
+        </div>{/* end space-y-6 content */}
         </div>{/* end scrollable editor */}
 
         {/* Preview panel */}
